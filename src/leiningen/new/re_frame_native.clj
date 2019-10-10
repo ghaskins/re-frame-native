7c84b5fd0f2dd9581db337d363580d05a6e18d55
Greg Haskins <gregory.haskins@gmail.com>
2019-10-10 15:38:11 -0400

    Checkpoint
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- src/leiningen/new/re_frame_native.clj
+++ src/leiningen/new/re_frame_native.clj
@@ -1,8 +1,13 @@
 (ns leiningen.new.re-frame-native
-  (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
-            [leiningen.core.main :as main]))
+  (:require [leiningen.new.templates :refer [renderer name-to-path ->files sanitize]]
+            [leiningen.core.main :as main]
+            [clojure.java.io :as io]
+            [clojure.string :as string]))
 
-(def render (renderer "re-frame-native"))
+(def prj-name "re-frame-native")
+(def render (renderer prj-name))
+
+(def template-dir (io/resource (string/join "/" ["leiningen" "new" (sanitize prj-name)])))
 
 (defn re-frame-native
   "FIXME: write documentation"
