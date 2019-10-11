03bb475c1e77d0b7efad9b7f9050a454bd5259ef
Greg Haskins <gregory.haskins@gmail.com>
2019-10-11 07:18:15 -0400

    Prepare for v1.0.1 development
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame-native/lein-template "1.0.0"
+(defproject re-frame-native/lein-template "1.0.1-SNAPSHOT"
   :description "A Leiningen template for generating a re-frame application on React Native"
   :url "https://github.com/ghaskins/re-frame-native"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
